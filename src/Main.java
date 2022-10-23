ce7bcbf530e94417ffc80807695dcaf59b77df55
nusubbotin <nu.subbotin@ya.ru>
2022-10-23 16:53:56 +0300

    часть 1. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
+        createCar();
+    }
 
+    private static void createCar() {
+        System.out.println("Создаем автопарк:");
+
+        Car ladaGranta = new Car("Lada", "Granta", 1.7f, "желтый", 2015, "России","механика", "внедорожник", "v123vv123", 4, Car.CAT_TIRES_SUMMER, 160);
+        Car.Insurance insurance = ladaGranta.new Insurance(2025, 5000, "123456789");
+        ladaGranta.setInsurance(insurance);
+        System.out.println(ladaGranta);
+
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3, "черный", 2020, "Германия","механика", "внедорожник", "n123nn123", 4, Car.CAT_TIRES_SUMMER, 220);
+        Car.Insurance audiInsurance = audi.new Insurance(2024, 10_000, "123456788");
+        audi.setInsurance(audiInsurance);
+        System.out.println(audi);
+
+        Car bmw = new Car("BMW", "Z8", 3, "черный", 2021, "Германия","механика", "внедорожник", "t123tt123", 4, Car.CAT_TIRES_SUMMER, 230);
+        Car.Insurance bmwInsurance = bmw.new Insurance(2024, 10_000, "123456777");
+        bmw.setInsurance(bmwInsurance);
+        System.out.println(bmw);
+
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4f, "красный", 2018, "Южная Корея","механика", "внедорожник", "5123fg123", 4, Car.CAT_TIRES_SUMMER, 180);
+        Car.Insurance kiaInsurance = kia.new Insurance(2023, 3000, "023456788");
+        kia.setInsurance(kiaInsurance);
+        System.out.println(kia);
+
+        Car hyundai = new Car("", "", 0, "", 0, "","механика", "внедорожник", "q123er123", 4, Car.CAT_TIRES_SUMMER, 180);
+        Car.Insurance hyundaiInsurance = hyundai.new Insurance(2024, 2000, "123456666");
+        hyundai.setInsurance(hyundaiInsurance);
+        System.out.println(hyundai);
+
+        hyundai.changeTires();
+        System.out.println("hyundai.getCarTires() = " + hyundai.getCarTires());
+
+        Car.Key key = hyundai.new Key(true, true);
+        hyundai.setKey(key);
+
+        //Car.Key key2 = hyundai.new Key(true, false);
+        //hyundai.setKey(key2);
     }
 }
---
+++ src/transport/Car.java
@@ -0,0 +1,212 @@
+package transport;
+
+import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Car extends Transport {
+
+    public static final String CAT_TIRES_SUMMER = "летние";
+    public static final String CAT_TIRES_WINTER = "зимние";
+
+    private float engineVolume;
+    private String transmission;
+    private String bodyType;
+    private String registrationNumber;
+    private int numberOfSeats;
+    private String carTires;
+
+    public class Key {
+        private boolean remoteEngineStart;
+        private boolean keylessEntry;
+
+        public Key(boolean remoteEngineStart, boolean keylessEntry) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessEntry = keylessEntry;
+        }
+
+        public boolean isRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public boolean isKeylessEntry() {
+            return keylessEntry;
+        }
+    }
+
+    public class Insurance {
+        private int validityYear;
+        private float price;
+        private String number;
+
+        public Insurance(int validityYear, float price, String number) {
+            if (validityYear <= 0) {
+                throw new IllegalArgumentException("Срок действия должен быть положительным целым числом");
+            }else {
+                this.validityYear = validityYear;
+            }
+
+            if (price <= 0) {
+                throw new IllegalArgumentException("Стоимость должена быть положительным числом");
+            }else {
+                this.price = price;
+            }
+
+            if (number == null || number.isEmpty()) {
+                throw new IllegalArgumentException("Номер страховки доджен быть заполнен");
+            } else if (!checkNumberLength(number)) {
+                throw new IllegalArgumentException("Номер страховки должен состоять из 9 символов");
+            } else {
+                this.number = number;
+            }
+        }
+
+        private boolean checkNumberLength(String number){
+            if (number != null && number.length() != 9){
+                return false;
+            }else {
+                return true;
+            }
+        }
+
+        public void  checkExpired() {
+            if (this.validityYear >= LocalDate.now().getYear()){
+                System.out.println("Срочно ехать оформлять новую страховку!");
+            }
+        }
+
+        @Override
+        public String toString() {
+            return  "validityYear= " + validityYear +
+                    ", price= " + price +
+                    ", number= " + number;
+        }
+    }
+
+    private Key key;
+
+    private Insurance insurance;
+
+    public Car(String brand, String model, float engineVolume, String color, int productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, int numberOfSeats, String carTires, int speed) {
+        super(brand, model, productionYear, productionCountry, color, speed);
+
+        if (bodyType == null || bodyType.isEmpty()) {
+            this.bodyType = "default";
+        }else {
+            this.bodyType = bodyType;
+        }
+
+        if (numberOfSeats == 0) {
+            this.numberOfSeats = 5;
+        }else {
+            this.numberOfSeats = numberOfSeats;
+        }
+
+        setEngineVolume(engineVolume);
+        setTransmission(transmission);
+        setRegistrationNumber(registrationNumber);
+        setCarTires(carTires);
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public float getEngineVolume() {
+        return engineVolume;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String getCarTires() {
+        return carTires;
+    }
+
+    public void setEngineVolume(float engineVolume) {
+        if (engineVolume == 0) {
+            this.engineVolume = 1.5f;
+        }else {
+            this.engineVolume = engineVolume;
+        }
+    }
+
+    public void setTransmission(String transmission) {
+        if (transmission == null || transmission.isEmpty()) {
+            this.transmission = "автомат";
+        }else {
+            this.transmission = transmission;
+        }
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        if (registrationNumber == null || registrationNumber.isEmpty()) {
+            throw new IllegalArgumentException("Регистрационный номер не может быть пустым");
+        } else if (!checkRegNumFormat(registrationNumber)) {
+            System.out.println("Регистрационный номер " + registrationNumber + " имеет не корректный формат. Допустимый формат х000хх000");
+        } else {
+            this.registrationNumber = registrationNumber;
+        }
+    }
+
+    public void setCarTires(String carTires) {
+        if (carTires == null || carTires.isEmpty()) {
+            this.carTires = CAT_TIRES_SUMMER;
+        } else if (carTires != CAT_TIRES_SUMMER && carTires != CAT_TIRES_WINTER) {
+            throw new IllegalArgumentException("Колеса может быть или \""+CAT_TIRES_SUMMER+"\" или \""+CAT_TIRES_WINTER+"\"");
+        } else {
+            this.carTires = carTires;
+        }
+    }
+
+    public void setKey(Key key) {
+        if (this.key == null) {
+            this.key = key;
+        }else {
+            throw new IllegalArgumentException("Параметры ключа уже установлены. Изменять запрещено!");
+        }
+    }
+
+    public void setInsurance(Insurance insurance) {
+        this.insurance = insurance;
+    }
+
+    public void changeTires() {
+        if (this.carTires == CAT_TIRES_SUMMER) {
+            this.carTires = CAT_TIRES_WINTER;
+        }else if (this.carTires == CAT_TIRES_WINTER) {
+            this.carTires = CAT_TIRES_SUMMER;
+        }
+    }
+
+    public boolean checkRegNumFormat(String registrationNumber){
+        Matcher m = Pattern.compile("[a-z](\\d){3}([a-z]){2}(\\d){3}").matcher(registrationNumber);
+        return m.find();
+    }
+
+    @Override
+    public String toString() {
+        return "Бренд: "+ this.brand
+                + "; Модель: " + this.model
+                + "; Объем двигателя: " + this.engineVolume
+                + "; Цвет кузова: " + this.color
+                + "; Год производства: " + this.productionYear
+                + "; Страна сборки: " + this.productionCountry
+                + "; Коробка передач: " + this.transmission
+                + "; Тип кузова: " + this.bodyType
+                + "; Регистрационный номер: " + this.registrationNumber
+                + "; Количество мест: " + this.numberOfSeats
+                + "; Тип резины: " + this.carTires
+                + "; Скорость: " + this.speed
+                + "; Сведения о страховке: (" + this.insurance + ")";
+    }
+}
